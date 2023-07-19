fb64f710e87669b4d451ada06ebb3a0382d3a1d8
Avneet Sekhon <avneetsekhon88@gmail.com>
2023-07-19 13:45:04 -0700

    Base project

---
+++ Motor Theory/Motor Theory/Motor_TheoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Motor_TheoryApp.swift
+//  Motor Theory
+//
+//  Created by Avneet Sekhon on 2023-07-19.
+//
+
+import SwiftUI
+
+@main
+struct Motor_TheoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
