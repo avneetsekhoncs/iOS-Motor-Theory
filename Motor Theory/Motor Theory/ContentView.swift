568b1bb82d4b0a5d61feb3ae11fd0e05f730ce76
Avneet Sekhon <avneetsekhon88@gmail.com>
2023-07-19 14:12:26 -0700

    Display make names in a list

--- Motor Theory/Motor Theory/ContentView.swift
+++ Motor Theory/Motor Theory/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // Creating the root view
+        NavigationStack {
+            // List presents rows from the data provided
+            List(motorVehicles) { mV in
+                // The Make's name fills each row
+                Text(mV.Make_Name)
+            }
+            
+            // Title of the current view
+            .navigationTitle("Motor Theory")
         }
-        .padding()
     }
 }
 
@@ -24,3 +28,16 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Each motor vehicle instance should provide this data
+struct motorVehicle: Identifiable {
+    let id: String
+    let Make_Name: String
+}
+
+// List of motor vehicle makes with a corresponding id
+let motorVehicles = [
+    motorVehicle(id: "1", Make_Name: "Audi"),
+    motorVehicle(id: "2", Make_Name: "Lamborghini"),
+    motorVehicle(id: "3", Make_Name: "Ferrari")
+]
